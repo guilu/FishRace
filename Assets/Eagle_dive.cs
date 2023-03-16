6a5be8a19d1b2508fa716f8708d321b44c26998d
Diego Barrio H <diegobarrioh@gmail.com>
2023-03-16 22:07:36 +0100

    eagle dive fix

--- Assets/Eagle_dive.cs
+++ Assets/Eagle_dive.cs
@@ -6,23 +6,28 @@ public class Eagle_dive : StateMachineBehaviour
 {
   Transform player;
   Rigidbody2D rb;
-  public float attackRange = 3f;
+  public float attackRange = 6f;
 
   // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
   override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
   {
+    Debug.Log("Entrando al behaviour " + animator.transform);
     player = GameObject.FindGameObjectWithTag("Player").transform;
     rb = animator.GetComponent<Rigidbody2D>();
+
   }
 
   // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
   override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
   {
+    float distancia = Vector3.Distance(player.position, rb.position);
 
-    if (Vector2.Distance(player.position, rb.position) <= attackRange)
+    Debug.Log("distancia hasta Player " + distancia);
+    if (distancia < attackRange)
     {
       animator.SetTrigger("attack");
     }
+
   }
 
   // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
